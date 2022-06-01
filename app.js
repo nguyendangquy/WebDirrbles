d1a2bf05e86fce193b8fff3e16ead8b85156dbd8
dangquy2000 <quy2k2k2k2k@gmail.com>
2022-06-01 10:17:56 +0700

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const
-  range = document.getElementById('range'),
-  rangeV = document.getElementById('rangeV'),
+const range = document.getElementById('range')
+const rangeV = document.getElementById('rangeV')
   setValue = ()=>{
-    const
-      newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) ),
-      newPosition = 10 - (newValue * 0.2);
+    const newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) )
+    const newPosition = 10 - (newValue * 0.2);
     rangeV.innerHTML = `<span>${range.value}</span>`;
     rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
-  };
+};
+
 document.addEventListener("DOMContentLoaded", setValue);
 range.addEventListener('input', setValue);
 
