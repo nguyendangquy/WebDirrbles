ae3af33c073878f5c91176b87e20d6d5e8547a09
dangquy2000 <quy2k2k2k2k@gmail.com>
2022-06-03 10:06:29 +0700

    convert scss

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-const range = document.getElementById('range')
-const rangeV = document.getElementById('rangeV')
-  setValue = ()=>{
-    const newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) )
-    const newPosition = 10 - (newValue * 0.2);
-    rangeV.innerHTML = `<span>${range.value}</span>`;
-    rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
+const range = document.getElementById("range");
+const rangeV = document.getElementById("rangeV");
+setValue = () => {
+  const newValue = Number(
+    ((range.value - range.min) * 100) / (range.max - range.min)
+  );
+  const newPosition = 10 - newValue * 0.2;
+  rangeV.innerHTML = `<span>${range.value}</span>`;
+  rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
 };
 
 document.addEventListener("DOMContentLoaded", setValue);
-range.addEventListener('input', setValue);
+range.addEventListener("input", setValue);
 
 /* Toogle navbar */
 const navBar = () => {
-  const navbar = document.querySelector('.icon-navbar')
-  const navMenu = document.querySelector('.header-menu')
+  const navbar = document.querySelector(".icon-navbar");
+  const navMenu = document.querySelector(".header-menu");
 
-  navbar.addEventListener('click', () => {
-    navMenu.classList.toggle('nav-active')
-  })
-
-}
-navBar()
+  navbar.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-active");
+  });
+};
+navBar();
 
 /* Toggle tab */
 const tabBar = () => {
-  const tabBar = document.getElementById('icon-tab')
-  const tabMenu = document.querySelector('.tab-lifestyle_nav')
+  const tabBar = document.getElementById("icon-tab");
+  const tabMenu = document.querySelector(".tab-lifestyle_nav");
 
-  tabBar.addEventListener('click', () => {
-    tabMenu.classList.toggle('tab-active')
-  })
-}
-tabBar()
+  tabBar.addEventListener("click", () => {
+    tabMenu.classList.toggle("tab-active");
+  });
+};
+tabBar();
